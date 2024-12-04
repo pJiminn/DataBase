897c39de742a6f591d8cd01929c7d0b26042dedf
pJiminn <p202201661@gmail.com>
2024-12-04 20:58:43 +0900

    첫 화면 만드는 중.. 버튼 눌렀을 때 들어가지게까지 해놓음git add .

--- app.py
+++ app.py
@@ -41,7 +41,19 @@ def movies():
     return render_template('movies.html', movies=movies)
 
 # 리뷰 페이지
+@app.route('/review')
+def review():
+    return render_template('review.html') 
 
+# 내 리뷰, 정보 볼 수 있는?
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')  # MY PROFILE 페이지
+
+# 그냥 만들어 봤어 4칸이 예뻐보여서 쩝쩝 하하
+@app.route('/about')
+def about():
+    return render_template('about.html')  # What's our app? 페이지
 
 @app.route('/reviews/<int:movie_id>', methods=['GET', 'POST'])
 def reviews(movie_id):
